5c82873fbbfa2b7590fcee4c41d6a7b4625bc4af
Jules Françoise <me@julesfrancoise.com>
2022-02-11 10:33:23 +0100

    Add Grad-CAM demos

---
+++ gradcam-pretrained/components/gradcam/gradcam.view.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { ViewContainer } from '@marcellejs/core';
+
+  export let title;
+  export let options;
+</script>
+
+<ViewContainer {title}>
+  <div>This is a <span class="my-color">custom</span> component with the following options:</div>
+  <p>{JSON.stringify(options)}</p>
+</ViewContainer>
+
+<style>
+  .my-color {
+    color: seagreen;
+  }
+
+</style>
